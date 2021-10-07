b98e27712bdb4f35d0f30cfe578935369e45b3fd
Cserkúti Péter <cserkuti.peter@encosoft-dev.hu>
2021-10-07 14:40:01 +0200

    Lab #1, Lab #2, Lab #3

---
+++ MyFirstSolution/LoggingExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace LoggingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //  Configuration, startup
+            Logger.WriteLog += message => Console.WriteLine(message);
+            Logger.WriteLog += message => Debug.WriteLine(message);
+            Logger.WriteLog += message => {
+                using (var sw = File.AppendText("output.txt"))
+                {
+                    sw.WriteLine(message);
+                    sw.Flush();
+                }
+                //var sw = File.AppendText("output.txt");
+                //try
+                //{
+                //    sw.WriteLine(message);
+                //    sw.Flush();
+                //}
+                //finally
+                //{
+                //    sw.Dispose();
+                //}
+            };
+
+
+            //  Somewhere else
+            try
+            {
+                throw new Exception("Test");
+            }
+            catch (Exception e)
+            {
+                Logger.Log(e.Message);
+            }
+        }
+    }
+
+    public static class Logger
+    {
+        public static event Action<string> WriteLog;
+
+        public static void Log(string message)
+        {
+            //if (WriteLog != null)
+            //    WriteLog(message);
+            WriteLog?.Invoke(message);
+        }
+    }
+}
