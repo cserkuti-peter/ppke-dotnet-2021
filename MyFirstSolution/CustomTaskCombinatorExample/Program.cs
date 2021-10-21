c873c964e0d7b6274c5cdfe6e91f5dacbd8d3442
Cserkúti Péter <cserkuti.peter@encosoft-dev.hu>
2021-10-21 17:30:08 +0200

    Lab #6 (Task combinators, Task.WhenAll, Task.WhenAny, TPL, Record)

---
+++ MyFirstSolution/CustomTaskCombinatorExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CustomTaskCombinatorExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await RetryOnFault(() => client.GetAsync("https://www.encosoftware.hu"), 5);
+            }
+        }
+
+        public static async Task<T> RetryOnFault<T>(Func<Task<T>> function, int maxTries)
+        {
+            for (int i = 0; i < maxTries; i++)
+            {
+                try { return await function(); }
+                catch { if (i == maxTries - 1) throw; }
+            }
+            return default(T);
+        }
+
+    }
+}
