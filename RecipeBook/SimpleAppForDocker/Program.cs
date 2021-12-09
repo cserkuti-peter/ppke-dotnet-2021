8616fce3d5e40264ffe27e5b8469b49935b49862
Cserkúti Péter <cserkuti.peter@encosoft-dev.hu>
2021-12-09 17:15:54 +0100

    Lab #12 - RecipeBook API - Docker

---
+++ RecipeBook/SimpleAppForDocker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleAppForDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
